dfc6dec0497299f3cd978a6a691d8d0d53be1471
Michael Coughlin <mcoughlin@logs.astro.caltech.edu>
2018-11-14 18:29:00 -0800

    Add plots

--- bin/kevin_test.py
+++ bin/kevin_test.py
@@ -6,10 +6,25 @@ import tables
 import pandas as pd
 import numpy as np
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+
 from pdtrend import FMdata, PDTrend
 
 dataDir = "/media/Data/Matchfiles/ztfweb.ipac.caltech.edu/ztf/ops/srcmatch/"
 directory="%s/*/*/*"%dataDir
+
+dataDir = "/media/Data/Matchfiles/ztfweb.ipac.caltech.edu/ztf/ops/srcmatch/rc18/fr000251-000300/"
+directory="%s/*"%dataDir
+
+#dataDir = "/media/Data/Matchfiles/ztfweb.ipac.caltech.edu/ztf/ops/srcmatch/rc02/fr000301-000350/"
+#directory="%s/*"%dataDir
+
+outputDir = "../output/detrend"
+if not os.path.isdir(outputDir):
+    os.makedirs(outputDir)
 
 lightcurves=[]
 baseline=0
@@ -17,14 +32,15 @@ p=0
 coordinates=[]
 
 for f in glob.iglob(directory):
+    #if not "ztf_000283_zr_c05_q3_match.pytable" in f: continue 
     with tables.open_file(f) as store:
         for tbl in store.walk_nodes("/", "Table"):
             if tbl.name in ["sourcedata", "transientdata"]:
                 group = tbl._v_parent
                 break
-        srcdata = pd.DataFrame.from_records(store.root.matches.sourcedata.read_where('programid == 2'))
+        srcdata = pd.DataFrame.from_records(store.root.matches.sourcedata.read_where('programid > 1'))
         srcdata.sort_values('matchid', axis=0, inplace=True)
-        exposures = pd.DataFrame.from_records(store.root.matches.exposures.read_where('programid == 2'))
+        exposures = pd.DataFrame.from_records(store.root.matches.exposures.read_where('programid > 1'))
         merged = srcdata.merge(exposures, on="expid")
 
         if len(merged.matchid.unique()) == 0:
@@ -36,18 +52,23 @@ for f in glob.iglob(directory):
 
         if len(idx) == 0:
             continue
- 
+
+        fname = f.split("/")[-1].replace("pytable","png") 
+
         matchids = matchids[idx]
         nmatchids = len(idx)
 
         lcs, times = [], []
-        for k in matchids:
+        #for k in matchids:
+        for k in matchids[:50]:
             df = merged[merged['matchid'] == k]
             RA = df.ra
             Dec = df.dec
             x = df.psfmag
             err=df.psfmagerr
             obsHJD = df.hjd            
+
+            print(np.min(obsHJD), np.max(obsHJD))
          
             if not len(x)>50:
                 continue
@@ -59,7 +80,6 @@ for f in glob.iglob(directory):
             coordinates.append(coordinate)
             lightcurve=(obsHJD.values,x.values,err.values)
             lightcurves.append(lightcurve)
-            print(p,len(x.values))
             p=p+1
 
             idx = np.argsort(obsHJD.values)
@@ -76,8 +96,13 @@ for f in glob.iglob(directory):
         # Find clusters and then construct master trends.
         pdt.run()
 
+        fig = plt.figure(figsize=(30,10))
+        ax = fig.add_subplot(1, 1, 1)
+        colors=cm.rainbow(np.linspace(0,1,len(lcs)))
+
         # Detrend each light curve.
-        for k,lc in zip(matchids,lcs):
+        cnt = 0
+        for k,lc in zip(matchids[:50],lcs[:50]):
             df = merged[merged['matchid'] == k]
             RA = df.ra
             Dec = df.dec
@@ -98,6 +123,25 @@ for f in glob.iglob(directory):
             lightcurve=(obsHJD.values,vals,err.values)
             lightcurves.append(lightcurve)
 
-        print(lightcurves)
-        print(stop) 
+            bins = np.linspace(np.min(vals),np.max(vals),11)
+            hist, bin_edges = np.histogram(vals, bins=bins, density=True)
+            bins = (bin_edges[1:] + bin_edges[:-1])/2.0
+            plt.plot(bins, hist, color = colors[cnt], linestyle='-', drawstyle='steps')
+
+            bins = np.linspace(np.min(x.values),np.max(x.values),11)
+            hist, bin_edges = np.histogram(x.values, bins=bins, density=True)
+            bins = (bin_edges[1:] + bin_edges[:-1])/2.0
+            plt.plot(bins, hist, color = colors[cnt], linestyle='--', drawstyle='steps')
+
+            #plt.scatter(obsHJD.values,x.values,s=20,marker='x',color=colors[cnt])
+            #plt.scatter(obsHJD.values,vals,s=20,marker='o',color=colors[cnt])
+
+            cnt = cnt+1
+
+        #ax.set_xscale('log')
+        ax.set_yscale('log')
+
+        plotName = os.path.join(outputDir,fname)
+        plt.savefig(plotName)
+        plt.close()
 
