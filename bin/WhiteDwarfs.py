c5b7dc5b6879eabcfce63797e168c372405caa92
Michael Coughlin <michael.coughlin@ligo.org>
2018-12-14 11:36:29 -0800

    Update whitedwarfs

--- bin/WhiteDwarfs.py
+++ bin/WhiteDwarfs.py
@@ -7,6 +7,7 @@ Created on Wed Oct 31 10:24:46 2018
 """
 
 import os
+import copy
 import numpy as np
 import glob
 import optparse
@@ -76,8 +77,9 @@ def kde_eval(kdedir,truth):
     return td
 
 def loadallmodels():
-    log10Teffmin, log10Teffmax = np.log10(40000), np.log10(70000)
-    log10Gmin, log10Gmax = 6, 8
+    log10Teffmin, log10Teffmax = np.log10(42000), np.log10(52000)
+    log10Gmin, log10Gmax = 7.6, 8.0
+    Mmin, Mmax = 0.2, 0.8
 
     FullData = np.empty((0,12))
     filenames = glob.glob(os.path.join(dataDir,'*.Z*'))
@@ -88,12 +90,16 @@ def loadallmodels():
         filenameSplit = filename.split('/')[-1].split(".")
         nameSplit = filenameSplit[0].split("_")
         solarMass = float(nameSplit[0][1:])*1e-1
+
+        if ((Mmin>solarMass) or (Mmax<solarMass)):
+            continue
+
         if nameSplit[1][0] == "E":
             fractionalMass = 1/float("1"+nameSplit[1])
         else:
             fractionalMass = 1/float(nameSplit[1])
         fractionalMass = np.log10(fractionalMass)
-        metallicity = 10**(-float(filenameSplit[1][1:]))     
+        metallicity = -float(filenameSplit[1][1:]) 
 
         lines = [line.rstrip('\n').rstrip('\r').rstrip('\x1a') for line in open(filename)]
         for line in lines:
@@ -118,77 +124,42 @@ def loadallmodels():
 
     return FullData
 
-def func(x,p0,p1,p2,vel0,vel1,v1,v2,g1,g2):
-        return p0+p1*x+p2*x**2+gaussian(v1,(1+(vel0)/300000)*(4340.462),v2)(x)+gaussian(g1,(1+vel1/300000)*(4340.462),g2)(x)
-        #return p0+p1*x+p2*x**2+lorentzian(v1,(1+(vel0)/300000)*(4340.462),v2)(x)+gaussian(g1,(1+vel1/300000)*(4340.462),g2)(x)
-
-def myprior_fit(cube, ndim, nparams):
-
-        cube[0] = cube[0]*2000.0
-        cube[1] = cube[1]*2000.0
-        cube[2] = cube[2]*2*np.pi
-        cube[3] = cube[3]*2000.0 - 1000.0
-        cube[4] = cube[4]*2000.0 - 1000.0
-
 def myprior(cube, ndim, nparams):
 
-        cube[0] = cube[0]*500 + 50
-        cube[1] = cube[1]*2.0 - 1.0
-        cube[2] = cube[2]*2e-4 - 1e-4
-        cube[3] = cube[3]*2000.0 - 1000.0
-        cube[4] = cube[4]*2000.0 - 1000.0
-        cube[5] = cube[5]*100.0 - 100.0
-        #cube[6] = cube[6]*25.0
-        cube[6] = cube[6]*1.0
-        cube[7] = cube[7]*100.0
-        cube[8] = cube[8]*25.0
+        cube[0] = cube[0]*0.6 + 0.2
+        cube[1] = cube[1]*5.0 - 5.0
+        cube[2] = cube[2]*3.0 - 3.0
+        cube[3] = cube[3]*20.0 + 0.0
 
-def myloglike_fit(cube, ndim, nparams):
+def myloglike(cube, ndim, nparams):
 
-    A = cube[0]
-    B = cube[1]
-    phi = cube[2]
-    c = cube[3]
-    d = cube[4]
+    mass = cube[0]
+    fractionalMass = cube[1]
+    metallicity = cube[2]   
+    logR = cube[3]
 
-    if c<d:
-        return -np.inf
+    param_list = [mass, fractionalMass, metallicity, logR]
+    param_list_postprocess = np.array(param_list)
+    for i in range(len(param_mins)):
+        param_list_postprocess[i] = (param_list_postprocess[i]-param_mins[i])/(param_maxs[i]-param_mins[i])
 
-    prob = 0
-    for key, color in zip(keys,colors):
-        vel0 = A*np.cos((2*(np.pi/12.0)*float(key))+phi)+c
-        vel1 = B*np.cos((2*(np.pi/12.0)*float(key))+phi+np.pi)+d
+    log10Teff_pred, log10Teff_sigma2 = gp_log10Teff.predict(np.atleast_2d(param_list_postprocess), return_std=True)
+    log10G_pred, log10G_sigma2 = gp_log10G.predict(np.atleast_2d(param_list_postprocess), return_std=True)
 
-        kdedir = data_out[key]["kdedir"]
-        vals = np.array([vel0,vel1]).T
-        kdeeval = kde_eval(kdedir,vals)[0]
-        prob = prob + np.log(kdeeval)
+    #vals = np.array([10**log10Teff_pred[0],log10G_pred[0]]).T
+    #kdeeval = kde_eval(kdedir_pts,vals)[0]
+    #prob = np.log(kdeeval)
+
+    T_mu, T_std = 47166, 887
+    T_prob = ss.norm(T_mu, T_std).logpdf(10**log10Teff_pred[0])
+
+    logg_mu, logg_std = 7.82, 0.061
+    logg_prob = ss.norm(logg_mu, logg_std).logpdf(log10G_pred[0])
+
+    prob = T_prob + logg_prob
 
     if np.isnan(prob):
         prob = -np.inf
-
-    return prob
-
-def myloglike(cube, ndim, nparams):
-    p0 = cube[0]
-    p1 = cube[1]
-    p2 = cube[2]
-    vel0 = cube[3]
-    vel1 = cube[4]
-    v1 = cube[5]
-    v2 = cube[6]
-    g1 = cube[7]
-    g2 = cube[8]
-
-    v2_mu, v2_std = 20.0, 3.0
-    v2 = ss.norm(v2_mu, v2_std).ppf(v2)
-
-    model = func(wavelengths,p0,p1,p2,vel0,vel1,v1,v2,g1,g2)
-    sigma = spec * errorbudget
-
-    x = model - spec
-    prob = ss.norm.logpdf(x, loc=0.0, scale=sigma)
-    prob = np.sum(prob)
 
     return prob
 
@@ -202,7 +173,7 @@ if not os.path.isdir(baseplotDir):
     os.makedirs(baseplotDir)
 
 newdat=loadallmodels()
-param_array = newdat[:,[0,8]]
+param_array = newdat[:,[0,1,2,8]]
 param_array_postprocess = np.array(param_array)
 param_mins, param_maxs = np.min(param_array_postprocess,axis=0),np.max(param_array_postprocess,axis=0)
 for i in range(len(param_mins)):
@@ -217,7 +188,42 @@ gp_log10Teff.fit(param_array_postprocess, log10Teff)
 gp_log10G = GaussianProcessRegressor(kernel=kernel,n_restarts_optimizer=0)
 gp_log10G.fit(param_array_postprocess, log10G)
 
-param_list = [0.5, 0.0]
+#T_mu, T_std = 47166, 887
+#T_unit = np.random.rand(1000)
+#T = ss.norm(T_mu, T_std).ppf(T_unit)
+
+#logg_mu, logg_std = 7.82, 0.061
+#logg_unit = np.random.rand(1000)
+#logg = ss.norm(logg_mu, logg_std).ppf(logg_unit)
+
+#pts = np.vstack((T,logg)).T
+#kdedir = greedy_kde_areas_2d(pts)
+#kdedir_pts = copy.deepcopy(kdedir)
+
+n_live_points = 5000
+evidence_tolerance = 0.1
+max_iter = 0
+title_fontsize = 26
+label_fontsize = 30
+
+parameters = ["Mass","fractionalMass","metallicity","logR"]
+labels = [r"$M$","frac","metal",r"$\log R$"]
+n_params = len(parameters)
+
+plotDir = os.path.join(baseplotDir,'com')
+if not os.path.isdir(plotDir):
+    os.makedirs(plotDir)
+
+pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False, resume = True, verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, outputfiles_basename='%s/2-'%plotDir, evidence_tolerance = evidence_tolerance, multimodal = False, max_iter = max_iter)
+
+multifile = "%s/2-post_equal_weights.dat"%plotDir
+data = np.loadtxt(multifile)
+
+mass, fractionalMass, metallicity, logR, loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4] 
+idx = np.argmax(loglikelihood)
+mass_best, fractionalMass_best, metallicity_best, logR_best = data[idx,0:-1]
+
+param_list = [mass_best, fractionalMass_best, metallicity_best, logR_best]
 param_list_postprocess = np.array(param_list)
 for i in range(len(param_mins)):
     param_list_postprocess[i] = (param_list_postprocess[i]-param_mins[i])/(param_maxs[i]-param_mins[i])
@@ -225,161 +231,12 @@ for i in range(len(param_mins)):
 log10Teff_pred, log10Teff_sigma2 = gp_log10Teff.predict(np.atleast_2d(param_list_postprocess), return_std=True)
 log10G_pred, log10G_sigma2 = gp_log10G.predict(np.atleast_2d(param_list_postprocess), return_std=True)
 
-print(log10Teff_pred, log10Teff_sigma2)
-print(log10G_pred, log10G_sigma2)
-print(stop)
+T_best = 10**log10Teff_pred[0]
+logg_best = log10G_pred[0]*1.0
 
-n=1
-final=[]
+print("Teff: %.5f, log(g): %.5f"%(T_best,logg_best))
 
-n_live_points = 100
-evidence_tolerance = 0.5
-max_iter = 0
-title_fontsize = 26
-label_fontsize = 30
-
-wavelengths, spectra = newdat[0], newdat[1:]
-#spectra = [spectra[3]]
-
-data_out = {}
-for ii,spec in enumerate(spectra):
-    parameters = ["p0","p1","p2","vel0","vel1","v1","v2","g1","g2"]
-    labels = [r"$p_0$",r"$p_1$",r"$p_2$",r"${\rm vel}_0$",r"${\rm vel}_1$",r"$v_1$",r"$v_2$",r"$g_1$",r"$g_2$"]
-    n_params = len(parameters)
-
-    plotDir = os.path.join(baseplotDir,'%d'%ii)
-    if not os.path.isdir(plotDir):
-        os.makedirs(plotDir)
-
-    pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False, resume = True, verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, outputfiles_basename='%s/2-'%plotDir, evidence_tolerance = evidence_tolerance, multimodal = False, max_iter = max_iter)
-
-    multifile = "%s/2-post_equal_weights.dat"%plotDir
-    data = np.loadtxt(multifile)
-
-    v2_mu, v2_std = 20.0, 3.0
-    v2 = ss.norm(v2_mu, v2_std).ppf(data[:,6])
-    data[:,6] = v2
-
-    p0,p1,p2,vel0,vel1,v1,v2,g1,g2,loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7], data[:,8], data[:,9]
-    idx = np.argmax(loglikelihood)
-    p0_best, p1_best, p2_best, vel0_best, vel1_best, v1_best, v2_best, g1_best, g2_best = data[idx,0:-1]
-    model = func(wavelengths,p0_best,p1_best,p2_best,vel0_best,vel1_best,v1_best,v2_best,g1_best,g2_best)
-
-    key = str(ii)
-    data_out[key] = {}
-    data_out[key]["spec"] = spec
-    data_out[key]["model"] = model
-    data_out[key]["wavelengths"] = wavelengths
-    data_out[key]["vel0"] = vel0
-    data_out[key]["vel1"] = vel1
-    pts = np.vstack((vel0,vel1)).T
-    data_out[key]["kdedir"] = greedy_kde_areas_2d(pts)
-
-    plotName = "%s/corner.pdf"%(plotDir)
-    if os.path.isfile(plotName): continue
-
-    figure = corner.corner(data[:,:-1], labels=labels,
-                       quantiles=[0.16, 0.5, 0.84],
-                       show_titles=True, title_kwargs={"fontsize": title_fontsize},
-                       label_kwargs={"fontsize": label_fontsize}, title_fmt=".3f",
-                       smooth=3)
-    figure.set_size_inches(18.0,18.0)
-    plt.savefig(plotName)
-    plt.close()
-
-    plotName = "%s/spec.pdf"%(plotDir)
-    fig = plt.figure(figsize=(22,28))
-    plt.plot(data_out[key]["wavelengths"],data_out[key]["spec"],'k--',linewidth=2)
-    plt.plot(data_out[key]["wavelengths"],data_out[key]["model"],'b-',linewidth=2)
-    plt.grid()
-    plt.yticks(fontsize=36)
-    plt.xticks(fontsize=36)
-    plt.savefig(plotName)
-    plt.close()
-
-keys = sorted(data_out.keys())
-colors=cm.rainbow(np.linspace(0,1,len(keys)))
-
-plotDir = os.path.join(baseplotDir,'combined')
-if not os.path.isdir(plotDir):
-    os.makedirs(plotDir)
-
-n_live_points = 10000
-evidence_tolerance = 0.1
-
-parameters = ["A","B","phi","c","d"]
-labels = ["A","B",r"$\phi$","c","d"]
-n_params = len(parameters)
-
-pymultinest.run(myloglike_fit, myprior_fit, n_params, importance_nested_sampling = False, resume = True, verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, outputfiles_basename='%s/2-'%plotDir, evidence_tolerance = evidence_tolerance, multimodal = False, max_iter = max_iter)
-
-multifile = "%s/2-post_equal_weights.dat"%plotDir
-data = np.loadtxt(multifile)
-A,B,phi,c,d,loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5]
-idx = np.argmax(loglikelihood)
-A_best, B_best, phi_best, c_best, d_best = data[idx,0:-1]
-
-color2 = 'coral'
-color1 = 'cornflowerblue'
-color3 = 'palegreen'
-color4 = 'darkmagenta'
-
-xs, vel0s, vel1s = [], np.zeros((len(keys),len(A))), np.zeros((len(keys),len(A))) 
-for jj,key in enumerate(keys):
-    x = float(key)
-    for ii in range(len(A)):
-        vel0s[jj,ii] = A[ii]*np.cos(2*(np.pi/12.0)*float(key)+phi[ii])+c[ii] 
-        vel1s[jj,ii] = B[ii]*np.cos(2*(np.pi/12.0)*float(key)+phi[ii]+np.pi)+d[ii]
-    xs.append(x)
-
-idx = np.argsort(xs)
-xs = np.array(xs)
-xs, vel0s, vel1s = xs[idx], vel0s[idx,:], vel1s[idx,:]
-
-vel0s_10 = np.percentile(vel0s,10,axis=1)
-vel0s_50 = np.percentile(vel0s,50,axis=1)
-vel0s_90 = np.percentile(vel0s,90,axis=1)
-
-vel1s_10 = np.percentile(vel1s,10,axis=1)
-vel1s_50 = np.percentile(vel1s,50,axis=1)
-vel1s_90 = np.percentile(vel1s,90,axis=1)
-
-plotName = "%s/violin.pdf"%(baseplotDir)
-fig = plt.figure(figsize=(22,28))
-for key, color in zip(keys,colors):
-    parts = plt.violinplot(data_out[key]["vel0"],[float(key)])
-    for partname in ('cbars','cmins','cmaxes'):
-        vp = parts[partname]
-        vp.set_edgecolor(color1)
-        vp.set_linewidth(1) 
-    for pc in parts['bodies']:
-        pc.set_facecolor(color1)
-        pc.set_edgecolor(color1)
-    parts = plt.violinplot(data_out[key]["vel1"],[float(key)])
-    for partname in ('cbars','cmins','cmaxes'):
-        vp = parts[partname]
-        vp.set_edgecolor(color2)
-        vp.set_linewidth(1)
-    for pc in parts['bodies']:
-        pc.set_facecolor(color2)
-        pc.set_edgecolor(color2)
-
-#plt.plot(xs,vel0s_10,'--',color=color1)
-plt.plot(xs,vel0s_50,'x--',color=color1)
-#plt.plot(xs,vel0s_90,'--',color=color1)
-#plt.plot(xs,vel1s_10,'--',color=color2)
-plt.plot(xs,vel1s_50,'o--',color=color2)
-#plt.plot(xs,vel1s_90,'--',color=color2)
-plt.fill_between(xs,vel0s_10,vel0s_90,facecolor=color1,edgecolor=color1,alpha=0.2,linewidth=3)
-plt.fill_between(xs,vel1s_10,vel1s_90,facecolor=color2,edgecolor=color2,alpha=0.2,linewidth=3)
-
-plt.grid()
-plt.yticks(fontsize=36)
-plt.xticks(fontsize=36)
-plt.savefig(plotName)
-plt.close()
-
-plotName = "%s/corner.pdf"%(baseplotDir)
+plotName = "%s/corner.pdf"%(plotDir)
 figure = corner.corner(data[:,:-1], labels=labels,
                        quantiles=[0.16, 0.5, 0.84],
                        show_titles=True, title_kwargs={"fontsize": title_fontsize},
@@ -389,39 +246,3 @@ figure.set_size_inches(18.0,18.0)
 plt.savefig(plotName)
 plt.close()
 
-print(stop)
-
-plotName = "%s/spec_panels.pdf"%(baseplotDir)
-fig = plt.figure(figsize=(22,28))
-
-cnt = 0
-for key, color in zip(keys,colors):
-    cnt = cnt+1
-    vals = "%d%d%d"%(len(keys),1,cnt)
-    if cnt == 1:
-        #ax1 = plt.subplot(eval(vals))
-        ax1 = plt.subplot(len(keys),1,cnt)
-    else:
-        #ax2 = plt.subplot(eval(vals),sharex=ax1,sharey=ax1)
-        ax2 = plt.subplot(len(keys),1,cnt,sharex=ax1,sharey=ax1)
-
-    plt.plot(data_out[key]["wavelengths"],data_out[key]["spec"],'k--',linewidth=2)
-    plt.plot(data_out[key]["wavelengths"],data_out[key]["model"],'b-',linewidth=2)
-
-    plt.ylabel('%.1f'%float(key),fontsize=48,rotation=0,labelpad=40)
-    #plt.xlim([start,stop])
-    #plt.ylim([0.0,1.0])
-    plt.grid()
-    plt.yticks(fontsize=36)
-
-    if (not cnt == len(keys)) and (not cnt == 1):
-        plt.setp(ax2.get_xticklabels(), visible=False)
-    elif cnt == 1:
-        plt.setp(ax1.get_xticklabels(), visible=False)
-    else:
-        plt.xticks(fontsize=36)
-
-ax1.set_zorder(1)
-#ax2.set_xlabel(r'$\lambda [\AA]$',fontsize=48,labelpad=30)
-plt.savefig(plotName)
-plt.close()
