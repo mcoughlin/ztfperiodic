04f943d3609ca2498670dd934acd24aeb748a80d
Michael Coughlin <coughlin@physics.harvard.edu>
2019-03-18 20:23:55 -0500

    Add more parameters to fit

--- papers/40minute/LightcurveAnalysis.py
+++ papers/40minute/LightcurveAnalysis.py
@@ -73,15 +73,15 @@ def basic_model(t,pars,grid='default'):
                 incl=pars[3],
                 t_zero=pars[4],
                 q=pars[7],
-                period=0.004800824101665522,
+                period=p,
                 shape_1='sphere',
-                shape_2='roche',
-                ldc_1=0.2,
-                ldc_2=0.4548,
-                gdc_2=0.61,
+                shape_2='sphere',
+                ldc_1=pars[8],
+                ldc_2=pars[9],
+                gdc_2=pars[10],
                 f_c=0,
                 f_s=0,
-                t_exp=3.0/86400,
+                t_exp=t_exp,
                 grid_1=grid,
                 grid_2=grid, heat_2 = pars[6],exact_grav=True,
                 verbose=0)
@@ -102,7 +102,10 @@ def myprior(cube, ndim, nparams):
         cube[4] = cube[4]*(tmax-tmin) + tmin
         cube[5] = cube[5]*1.0 + 0.0
         cube[6] = cube[6]*10.0 + 0.0
-        cube[7] = cube[7]*1.0 + 0.0
+        cube[7] = cube[7]*2.0 + 0.0
+        cube[8] = cube[8]*0.4 + 0.0
+        cube[9] = cube[9]*0.4 + 0.0
+        cube[10] = cube[10]*0.2 + 0.3
 
 def myloglike(cube, ndim, nparams):
 
@@ -114,8 +117,11 @@ def myloglike(cube, ndim, nparams):
     scale = cube[5]
     heat_2 = cube[6]
     q = cube[7]
+    ldc_1=cube[8]
+    ldc_2=cube[9]
+    gdc_2=cube[10]
 
-    model_pars = [r1,r2,J,i,t0,scale,heat_2,q]
+    model_pars = [r1,r2,J,i,t0,scale,heat_2,q,ldc_1,ldc_2,gdc_2]
     model = basic_model(t[:],model_pars)
  
     x = model - y
@@ -152,17 +158,18 @@ t=data[:,0]
 RA, Dec = 285.3559192, 53.1581982
 t = BJDConvert(t, RA, Dec)
 t = np.array([x.value for x in t])
+t_exp=t[1]-t[0]
 
-r1 = 0.3
+r1 = 0.125
 r2 = 0.3
 J = 1/15.0
-i = 75
+i = 85
 t0 = t[0]
 p = 0.029166666666666667
 scale = np.median(y)/1.3
 heat_2 = 5
 bfac_2 = 1.30
-q=0.4
+q=0.3
 ldc_1=0.2
 ldc_2=0.4548
 gdc_2=0.61
@@ -174,7 +181,7 @@ tmin, tmax = np.min(t), np.min(t)+p
 
 # generate the test light curve given parameters
 
-model_pars = [r1,r2,J,i,t0,scale,heat_2,q] # the parameters
+model_pars = [r1,r2,J,i,t0,scale,heat_2,q,ldc_1,ldc_2,gdc_2] # the parameters
 
 # and add errors
 
@@ -202,8 +209,8 @@ max_iter = 0
 title_fontsize = 26
 label_fontsize = 30
 
-parameters = ["r1","r2","J","i","t0","scale","heat_2","q"]
-labels = [r"$r_1$",r"$r_2$","J","i",r"$t_0$","scale",r"${\rm heat}_2$","q"]
+parameters = ["r1","r2","J","i","t0","scale","heat_2","q","ldc_1","ldc_2","gdc_2"]
+labels = [r"$r_1$",r"$r_2$","J","i",r"$t_0$","scale",r"${\rm heat}_2$","q","$ldc_1$","$ldc_2$","$gdc_2$"]
 n_params = len(parameters)
 
 plotDir = os.path.join(baseplotDir,'posteriors')
@@ -215,9 +222,9 @@ pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False
 multifile = "%s/2-post_equal_weights.dat"%plotDir
 data = np.loadtxt(multifile)
 
-r1, r2, J, i, t0,scale, heat_2, q, loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4] 
+r1, r2, J, i, t0,scale, heat_2, q, ldc_1, ldc_2, gdc_2, loglikelihood = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7], data[:,8], data[:,9], data[:,10], data[:,11] 
 idx = np.argmax(loglikelihood)
-r1_best, r2_best, J_best, i_best, t0_best, scale_best, heat_2_best, q_best = data[idx,0:-1]
+r1_best, r2_best, J_best, i_best, t0_best, scale_best, heat_2_best, q_best, ldc_1_best, ldc_2_best, gdc_2_best = data[idx,0:-1]
 
 plotName = "%s/corner.pdf"%(plotDir)
 figure = corner.corner(data[:,:-1], labels=labels,
@@ -226,6 +233,21 @@ figure = corner.corner(data[:,:-1], labels=labels,
                        label_kwargs={"fontsize": label_fontsize}, title_fmt=".3f",
                        smooth=3)
 figure.set_size_inches(18.0,18.0)
+plt.savefig(plotName)
+plt.close()
+
+model_pars = [r1_best, r2_best, J_best, i_best, t0_best, scale_best, heat_2_best, q_best, ldc_1_best, ldc_2_best, gdc_2_best] # the parameters
+
+plt.figure()
+# lets have a look:
+plt.errorbar(lc[:,0],lc[:,1],lc[:,2],fmt='k.')
+plt.ylabel('flux')
+plt.xlabel('time')
+# my initial guess (r1,r2,J,i,t0,p,scale)
+guess = model_pars
+plt.plot(t[:],basic_model(t[:],model_pars),zorder=4)
+plt.show()
+plotName = os.path.join(baseplotDir,'fit.png')
 plt.savefig(plotName)
 plt.close()
 
