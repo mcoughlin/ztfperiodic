4de75917cd6a16c1f2ff80dc181b47a519d758e4
Michael Coughlin <michael.coughlin@ligo.org>
2020-06-23 19:59:42 -0700

    condorize object classification

---
+++ bin/ztfperiodic_classify_objects.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python
+
+import os, sys
+import tempfile
+import time
+import glob
+import optparse
+import pickle
+from functools import partial
+
+import tables
+import pandas as pd
+import numpy as np
+import h5py
+from scipy.signal import lombscargle
+
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.rcParams.update({'font.size': 16})
+matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astroquery.sdss import SDSS
+import astropy.constants as const
+import astropy.io.fits
+
+import ztfperiodic
+from ztfperiodic.utils import get_kowalski_features
+from ztfperiodic.classify import classify
+
+try:
+    from penquins import Kowalski
+except:
+    print("penquins not installed... need to use matchfiles.")
+
+def parse_commandline():
+    """
+    Parse the options given on the command-line.
+    """
+    parser = optparse.OptionParser()
+    parser.add_option("--doPlots",  action="store_true", default=False)
+
+    parser.add_option("--doParallel",  action="store_true", default=False)
+    parser.add_option("-n","--Ncore",default=4,type=int)
+
+    parser.add_option("--doQuadrantFile",  action="store_true", default=False)
+    parser.add_option("--quadrant_file",default="../input/quadrant_file.dat")
+    parser.add_option("--quadrant_index",default=0,type=int)
+
+    parser.add_option("-o","--outputDir",default="/home/michael.coughlin/ZTF/output")
+    parser.add_option("-m","--modelFile",default="/home/michael.coughlin/ZTF/ZTFVariability/pipeline/saved_models/d11.ea.f.model") 
+
+    parser.add_option("-k","--kowalski_batch_size",default=1000,type=int)
+    parser.add_option("-a","--algorithm",default="xgboost")
+
+    parser.add_option("-l","--lightcurve_source",default="Kowalski")
+    parser.add_option("-s","--source_type",default="quadrant")
+    parser.add_option("--catalog_file",default="../catalogs/swift.dat")
+    parser.add_option("--Ncatalog",default=1,type=int)
+    parser.add_option("--Ncatindex",default=0,type=int)
+
+    parser.add_option("-u","--user")
+    parser.add_option("-w","--pwd")
+
+    opts, args = parser.parse_args()
+
+    return opts
+
+def touch(fname):
+    if os.path.exists(fname):
+        os.utime(fname, None)
+    else:
+        open(fname, 'a').close()
+
+# Parse command line
+opts = parse_commandline()
+
+if not opts.lightcurve_source in ["Kowalski"]:
+    print("--lightcurve_source must be either Kowalski")
+    exit(0)
+
+algorithm = opts.algorithm
+outputDir = opts.outputDir
+catalog_file = opts.catalog_file
+quadrant_file = opts.quadrant_file
+Ncatalog = opts.Ncatalog
+Ncatindex = opts.Ncatindex
+modelFile = opts.modelFile
+
+if opts.doQuadrantFile:
+    if opts.lightcurve_source == "Kowalski":
+        quad_out = np.loadtxt(quadrant_file)
+        idx = np.where(quad_out[:,0] == opts.quadrant_index)[0]
+        row = quad_out[idx,:][0]
+        field, ccd, quadrant = row[1], row[2], row[3]
+        Ncatindex, Ncatalog = row[4], row[5]
+    elif opts.lightcurve_source == "matchfiles":
+        lines = [line.rstrip('\n') for line in open(quadrant_file)]
+        for line in lines:
+            lineSplit = list(filter(None,line.split(" ")))
+            if int(lineSplit[0]) == opts.quadrant_index:
+                matchFile = lineSplit[1]
+                print("Using matchfile %s" % matchFile)
+                Ncatindex = int(lineSplit[2])
+
+scriptpath = os.path.realpath(__file__)
+starCatalogDir = os.path.join("/".join(scriptpath.split("/")[:-2]),"catalogs")
+inputDir = os.path.join("/".join(scriptpath.split("/")[:-2]),"input")
+
+catalogDir = os.path.join(outputDir,'catalog',algorithm)
+if (opts.source_type == "catalog") and ("fermi" in catalog_file):
+    catalogDir = os.path.join(catalogDir,'%d' % Ncatindex)
+modelName = modelFile.replace(".model","").split("/")[-1]
+catalogDir = os.path.join(catalogDir, modelName)
+if not os.path.isdir(catalogDir):
+    os.makedirs(catalogDir)
+
+lightcurves = []
+coordinates = []
+baseline=0
+fil = 'all'
+
+print('Organizing lightcurves...')
+if opts.lightcurve_source == "Kowalski":
+
+    catalogFile = os.path.join(catalogDir,"%d.h5"%(Ncatindex))
+
+    kow = []
+    nquery = 10
+    cnt = 0
+    while cnt < nquery:
+        try:
+            kow = Kowalski(username=opts.user, password=opts.pwd)
+            break
+        except:
+            time.sleep(5)
+        cnt = cnt + 1
+    if cnt == nquery:
+        raise Exception('Kowalski connection failed...')
+
+    if opts.source_type == "quadrant":
+        catalogFile = os.path.join(catalogDir,"%d.h5"%(Ncatindex))
+
+        ids, features = get_kowalski_features(kow, 
+                                              num_batches=Ncatalog,
+                                              nb=Ncatindex)
+
+    elif opts.source_type == "catalog":
+
+        amaj, amin, phi = None, None, None
+        if not opts.default_err is None:
+            default_err = opts.default_err
+        else:
+            if doCombineFilt:
+                default_err = 3.0
+            else:
+                default_err = 5.0
+
+        if ".dat" in catalog_file:
+            lines = [line.rstrip('\n') for line in open(catalog_file)]
+            names, ras, decs, errs = [], [], [], []
+            if ("fermi" in catalog_file):
+                amaj, amin, phi = [], [], []
+            for line in lines:
+                lineSplit = list(filter(None,line.split(" ")))
+                if ("blue" in catalog_file) or ("uvex" in catalog_file) or ("xraybinary" in catalog_file):
+                    ra_hex, dec_hex = convert_to_hex(float(lineSplit[0])*24/360.0,delimiter=''), convert_to_hex(float(lineSplit[1]),delimiter='')
+                    if dec_hex[0] == "-":
+                        objname = "ZTFJ%s%s"%(ra_hex[:4],dec_hex[:5])
+                    else:
+                        objname = "ZTFJ%s%s"%(ra_hex[:4],dec_hex[:4])
+                    names.append(objname)
+                    ras.append(float(lineSplit[0]))
+                    decs.append(float(lineSplit[1]))
+                    errs.append(default_err)
+                elif "gaia_large_rv.dat" in catalog_file:
+                    ra_hex, dec_hex = convert_to_hex(float(lineSplit[0])*24/360.0,delimiter=''), convert_to_hex(float(lineSplit[1]),delimiter='')
+                    if dec_hex[0] == "-":
+                        objname = "ZTFJ%s%s"%(ra_hex[:4],dec_hex[:5])
+                    else:
+                        objname = "ZTFJ%s%s"%(ra_hex[:4],dec_hex[:4])
+                    names.append(objname)
+                    ras.append(float(lineSplit[0]))
+                    decs.append(float(lineSplit[1]))
+                    errs.append(default_err)
+                elif ("vlss" in catalog_file):
+                    names.append(lineSplit[0])
+                    ras.append(float(lineSplit[1]))
+                    decs.append(float(lineSplit[2]))
+                    err = np.sqrt(float(lineSplit[3])**2 + float(lineSplit[4])**2)*3600.0
+                    errs.append(err)
+                elif ("apogee" in catalog_file):
+                    names.append(lineSplit[0])
+                    ras.append(float(lineSplit[3]))
+                    decs.append(float(lineSplit[4]))
+                    errs.append(default_err)
+                elif ("fermi" in catalog_file):
+                    names.append(lineSplit[0])
+                    ras.append(float(lineSplit[1]))
+                    decs.append(float(lineSplit[2]))
+                    err = np.sqrt(float(lineSplit[3])**2 + float(lineSplit[4])**2)*3600.0
+                    errs.append(err)
+                    amaj.append(float(lineSplit[3]))
+                    amin.append(float(lineSplit[4]))
+                    phi.append(float(lineSplit[5]))
+                elif ("swift" in catalog_file) or ("xmm" in catalog_file):
+                    names.append(lineSplit[0])
+                    ras.append(float(lineSplit[1]))
+                    decs.append(float(lineSplit[2]))
+                    err = float(lineSplit[3])
+                    errs.append(err)
+                else:
+                    names.append(lineSplit[0])
+                    ras.append(float(lineSplit[1]))
+                    decs.append(float(lineSplit[2]))
+                    errs.append(default_err)
+            names = np.array(names)
+            ras, decs, errs = np.array(ras), np.array(decs), np.array(errs)
+            if ("fermi" in catalog_file):
+                amaj, amin, phi = np.array(amaj), np.array(amin), np.array(phi)
+        elif ".hdf5" in catalog_file:
+            with h5py.File(catalog_file, 'r') as f:
+                ras, decs = f['ra'][:], f['dec'][:]
+            errs = default_err*np.ones(ras.shape)
+
+            names = []
+            for ra, dec in zip(ras, decs):
+                ra_hex, dec_hex = convert_to_hex(ra*24/360.0,delimiter=''), convert_to_hex(dec,delimiter='')
+                if dec_hex[0] == "-":
+                    objname = "ZTFJ%s%s"%(ra_hex[:4],dec_hex[:5])
+                else:
+                    objname = "ZTFJ%s%s"%(ra_hex[:4],dec_hex[:4])
+                names.append(objname)
+            names = np.array(names)
+
+        if opts.doRemoveBrightStars:
+            filename = "%s/bsc5.hdf5" % inputDir
+            sep = brightstardist(filename,ras,decs)
+            idx1 = np.where(sep >= opts.stardist)[0]
+            filename = "%s/Gaia.hdf5" % inputDir
+            sep = brightstardist(filename,ras,decs)
+            idx2 = np.where(sep >= opts.stardist)[0]
+            idx = np.union1d(idx1,idx2)
+            names = names[idx]
+            ras, decs, errs = ras[idx], decs[idx], errs[idx]
+            if ("fermi" in catalog_file):
+                amaj, amin, phi = amaj[idx], amin[idx], phi[idx]
+
+        names_split = np.array_split(names,Ncatalog)
+        ras_split = np.array_split(ras,Ncatalog)
+        decs_split = np.array_split(decs,Ncatalog)
+        errs_split = np.array_split(errs,Ncatalog)
+
+        names = names_split[Ncatindex]
+        ras = ras_split[Ncatindex]
+        decs = decs_split[Ncatindex]
+        errs = errs_split[Ncatindex]
+
+        if ("fermi" in catalog_file):
+            amaj_split = np.array_split(amaj,Ncatalog)
+            amin_split = np.array_split(amin,Ncatalog)
+            phi_split = np.array_split(phi,Ncatalog)
+
+            amaj = amaj_split[Ncatindex]
+            amin = amin_split[Ncatindex]
+            phi = phi_split[Ncatindex]
+
+        catalog_file_split = catalog_file.replace(".dat","").replace(".hdf5","").replace(".h5","").split("/")[-1]
+        catalogFile = os.path.join(catalogDir,"%s_%d.h5"%(catalog_file_split,
+                                                           Ncatindex))
+        if opts.doSpectra:
+            spectraFile = os.path.join(spectraDir,"%s_%d.pkl"%(catalog_file_split,
+                                                               Ncatindex))
+
+        if doSimulateLightcurves:
+            lightcurves, coordinates, filters, ids,\
+            absmags, bp_rps, names, baseline =\
+                get_simulated_list(ras, decs,
+                                  min_epochs=min_epochs,
+                                  names=names,
+                                  doCombineFilt=doCombineFilt,
+                                  doRemoveHC=doRemoveHC,
+                                  doUsePDot=doUsePDot)
+        else:
+            lightcurves, coordinates, filters, ids,\
+            absmags, bp_rps, names, baseline =\
+                get_kowalski_list(ras, decs,
+                                  kow,
+                                  program_ids=program_ids,
+                                  min_epochs=min_epochs,
+                                  errs=errs,
+                                  names=names,
+                                  amaj=amaj, amin=amin, phi=phi,
+                                  doCombineFilt=doCombineFilt,
+                                  doRemoveHC=doRemoveHC,
+                                  doExtinction=doExtinction,
+                                  doSigmaClipping=doSigmaClipping,
+                                  sigmathresh=sigmathresh,
+                                  doOutbursting=doOutbursting,
+                                  doCrossMatch=doCrossMatch,
+                                  crossmatch_radius=crossmatch_radius)
+
+    elif opts.source_type == "objid":
+
+        if (".dat" in catalog_file) or (".txt" in catalog_file):
+            objids = np.loadtxt(catalog_file)
+        else:
+            print("Sorry I don't know this file extension...")
+            exit(0)
+        objids = objids[:,0]         
+        objids_split = np.array_split(objids,Ncatalog)
+        objids = objids_split[Ncatindex]
+
+        catalog_file_split = catalog_file.replace(".dat","").replace(".hdf5","").replace(".h5","").split("/")[-1]
+        catalogFile = os.path.join(catalogDir,"%s_%d.h5"%(catalog_file_split,
+                                                           Ncatindex))
+
+        if opts.doSpectra:
+            spectraFile = os.path.join(spectraDir,"%s_%d.pkl"%(catalog_file_split,
+                                                               Ncatindex))
+
+
+        lightcurves, coordinates, filters, ids,\
+        absmags, bp_rps, names, baseline =\
+            get_kowalski_objids(objids, kow,
+                                program_ids=program_ids,
+                                min_epochs=min_epochs,
+                                doRemoveHC=doRemoveHC,
+                                doExtinction=doExtinction,
+                                doSigmaClipping=doSigmaClipping,
+                                sigmathresh=sigmathresh,
+                                doOutbursting=doOutbursting)
+    else:
+        print("Source type unknown...")
+        exit(0)
+
+if len(features) == 0:
+    touch(catalogFile)
+    print('No features available... exiting.')
+    exit(0)
+
+print('Analyzing %d lightcurves...' % len(features))
+start_time = time.time()
+
+pred = classify(algorithm, features, modelFile=modelFile)
+
+end_time = time.time()
+print('Lightcurve analysis took %.2f seconds' % (end_time - start_time))
+
+data_out = np.vstack([ids, pred]).T
+with h5py.File(catalogFile, 'w') as hf:
+    hf.create_dataset("preds",  data=data_out)
+
+
---
+++ bin/ztfperiodic_classify_objects_condor.py
@@ -0,0 +1,152 @@
+
+import os, sys
+import glob
+import optparse
+
+import tables
+import pandas as pd
+import numpy as np
+import h5py
+
+import ztfperiodic.utils
+
+try:
+    from penquins import Kowalski
+except:
+    print("penquins not installed... need to use matchfiles.")
+
+def parse_commandline():
+    """
+    Parse the options given on the command-line.
+    """
+    parser = optparse.OptionParser()
+
+    parser.add_option("-p","--python",default="python")
+
+    parser.add_option("-o","--outputDir",default="/home/mcoughlin/ZTF/output")
+    parser.add_option("-b","--batch_size",default=1,type=int)
+    parser.add_option("-a","--algorithm",default="xgboost")
+
+    parser.add_option("--doQuadrantScale",  action="store_true", default=False)
+
+    parser.add_option("-l","--lightcurve_source",default="Kowalski")
+    parser.add_option("-s","--source_type",default="quadrant")
+    parser.add_option("--catalog_file",default="../input/xray.dat")
+    parser.add_option("--Ncatalog",default=13.0,type=int)
+    parser.add_option("--Nmax",default=10000.0,type=int)
+
+    parser.add_option("-m","--modelPath",default="/home/michael.coughlin/ZTF/ZTFVariability/pipeline/saved_models/")
+
+    parser.add_option("--doDocker",  action="store_true", default=False)
+
+    parser.add_option("-u","--user")
+    parser.add_option("-w","--pwd")
+
+    opts, args = parser.parse_args()
+
+    return opts
+
+# Parse command line
+opts = parse_commandline()
+Ncatalog = opts.Ncatalog
+
+outputDir = opts.outputDir
+batch_size = opts.batch_size
+algorithm = opts.algorithm
+modelPath = opts.modelPath
+
+catalogDir = os.path.join(outputDir,'catalog',algorithm)
+
+condorDir = os.path.join(outputDir,'condor')
+if not os.path.isdir(condorDir):
+    os.makedirs(condorDir)
+
+logDir = os.path.join(condorDir,'logs')
+if not os.path.isdir(logDir):
+    os.makedirs(logDir)
+
+modelFiles = glob.glob(os.path.join(modelPath, "d11*.f.*model"))
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+condordag = os.path.join(condorDir,'condor.dag')
+fid = open(condordag,'w') 
+condorsh = os.path.join(condorDir,'condor.sh')
+fid1 = open(condorsh,'w') 
+
+job_number = 0
+
+if opts.doQuadrantScale:
+    kow = Kowalski(username=opts.user, password=opts.pwd)
+
+if opts.lightcurve_source == "Kowalski":
+
+    if opts.source_type == "quadrant":
+        if opts.doQuadrantScale:
+            #qu = {"query_type":"count_documents",
+            #      "query": {
+            #          "catalog": 'ZTF_source_features_20191101',
+            #          "filter": {}
+            #          }
+            #     }
+            #r = ztfperiodic.utils.database_query(kow, qu, nquery = 1)
+            #nlightcurves = r['result_data']['query_result']
+            nlightcurves = 1000000
+            Ncatalog = int(np.ceil(float(nlightcurves)/opts.Nmax))
+
+        for modelFile in modelFiles: 
+            modelName = modelFile.replace(".model","").split("/")[-1]
+            for ii in range(Ncatalog):
+                catalogFile = os.path.join(catalogDir,modelName, "%d.h5"%(ii))
+                if os.path.isfile(catalogFile):
+                    print('%s already exists... continuing.' % catalogFile)
+                    continue
+    
+                if opts.doDocker:
+                    fid1.write('nvidia-docker run --runtime=nvidia python-ztfperiodic --outputDir %s --program_ids 1,2,3 --field %d --ccd %d --quadrant %d --user %s --pwd %s --batch_size %d -l Kowalski --source_type quadrant --Ncatalog %d --Ncatindex %d --algorithm %s --doRemoveTerrestrial --doPlots --doRemoveBrightStars --doLightcurveStats %s\n'%(outputDir, field, ccd, quadrant, opts.user, opts.pwd,opts.batch_size, Ncatalog, ii, opts.algorithm, extra_flags))
+                else:
+                    fid1.write('%s %s/ztfperiodic_classify_objects.py --outputDir %s --user %s --pwd %s -l Kowalski --source_type quadrant --Ncatalog %d --Ncatindex %d --algorithm %s --doPlots --modelFile %s\n'%(opts.python, dir_path, outputDir, opts.user, opts.pwd, Ncatalog, ii, opts.algorithm, modelFile))
+        
+                fid.write('JOB %d condor.sub\n'%(job_number))
+                fid.write('RETRY %d 3\n'%(job_number))
+                fid.write('VARS %d jobNumber="%d" Ncatindex="%d" Ncatalog="%d" modelFile="%s"\n'%(job_number,job_number,ii, Ncatalog, modelFile))
+                fid.write('\n\n')
+                job_number = job_number + 1
+
+    elif opts.source_type == "catalog":
+        for ii in range(Ncatalog):
+            if opts.doDocker:
+                fid1.write('nvidia-docker run --runtime=nvidia python-ztfperiodic %s --outputDir %s --user %s --pwd %s --batch_size %d -l Kowalski --source_type catalog --algorithm %s --doRemoveTerrestrial --doRemoveBrightStars --stardist 13.0 --program_ids 1,2,3 --catalog_file %s --doLightcurveStats --doPlots --Ncatalog %d --Ncatindex %d %s\n'%(cpu_gpu_flag, outputDir, opts.user, opts.pwd,opts.batch_size, opts.algorithm, opts.catalog_file,opts.Ncatalog,ii,extra_flags))
+            else:
+                fid1.write('%s %s/ztfperiodic_classify_objects.py %s --outputDir %s --user %s --pwd %s -l Kowalski --source_type catalog --algorithm %s --doRemoveTerrestrial --doRemoveBrightStars --stardist 13.0 --program_ids 1,2,3 --catalog_file %s --doLightcurveStats --doPlots --Ncatalog %d --Ncatindex %d %s\n'%(opts.python, dir_path, cpu_gpu_flag, outputDir, opts.user, opts.pwd,opts.batch_size, opts.algorithm, opts.catalog_file,opts.Ncatalog,ii,extra_flags))
+
+            fid.write('JOB %d condor.sub\n'%(job_number))
+            fid.write('RETRY %d 3\n'%(job_number))
+            fid.write('VARS %d jobNumber="%d" Ncatindex="%d" Ncatalog="%d"\n'%(job_number,job_number, ii, Ncatalog))
+            fid.write('\n\n')
+            job_number = job_number + 1
+
+fid1.close()
+fid.close()
+
+fid = open(os.path.join(condorDir,'condor.sub'),'w')
+fid.write('executable = %s/ztfperiodic_classify_objects.py\n'%dir_path)
+fid.write('output = logs/out.$(jobNumber)\n');
+fid.write('error = logs/err.$(jobNumber)\n');
+if opts.lightcurve_source == "Kowalski":
+    if opts.source_type == "quadrant":
+        fid.write('arguments = --outputDir %s --Ncatalog $(Ncatalog) --Ncatindex $(Ncatindex) --user %s --pwd %s -l Kowalski --doPlots --algorithm %s --modelFile $(modelFile)\n'%(outputDir,opts.user,opts.pwd,opts.algorithm))
+    elif opts.source_type == "catalog":
+        fid.write('arguments = %s --outputDir %s --batch_size %d --user %s --pwd %s -l Kowalski --doSaveMemory --doRemoveTerrestrial --source_type catalog --catalog_file %s --doRemoveBrightStars --stardist 13.0 --program_ids 1,2,3 --doPlots --Ncatalog %d --Ncatindex $(Ncatindex) --algorithm %s %s\n'%(outputDir,batch_size,opts.user,opts.pwd,opts.catalog_file,opts.Ncatalog,opts.algorithm,extra_flags))
+fid.write('requirements = OpSys == "LINUX"\n');
+fid.write('request_memory = 8192\n');
+fid.write('request_cpus = 1\n');
+fid.write('accounting_group = ligo.dev.o2.burst.allsky.stamp\n');
+fid.write('notification = never\n');
+fid.write('getenv = true\n');
+fid.write('log = /local/michael.coughlin/folding.log\n')
+fid.write('+MaxHours = 24\n');
+fid.write('universe = vanilla\n');
+fid.write('queue 1\n');
+fid.close()
+
